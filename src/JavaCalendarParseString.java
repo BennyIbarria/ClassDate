b8a7dcfeef7643401f451367de31a306fdb6fbd9
Beny300484 <topeteplay89@gmail.com>
2022-09-29 00:05:16 -0500

    EjemplosClaseDate

--- src/JavaCalendarParseString.java
+++ src/JavaCalendarParseString.java
@@ -19,6 +19,32 @@ public class JavaCalendarParseString {
         //Imprimimos la fecha capturada por el usuario en tipo cadena de texto
         System.out.println("fecha = " + fecha);
         //Imprimimos la fecha capturada por el usuario en formato SimpleDateFormat
-        System.out.println("format = " + format.format(fecha));
+        System.out.println("Formato de fecha = " + format.format(fecha));
+
+        //Comparando fechas
+        //Creamos una nueva instancia de la clase Date
+        Date fecha2 = new Date();
+        //Imprimimos la fecha 2
+        System.out.println("La fecha de hoy es: " + fecha2);
+
+        //Comparamos la fecha capturada por el usuario vs la fecha actual que maneja el sistema
+        if(fecha.after(fecha2)){
+            System.out.println("Fecha capturada es posterior a la fecha actual");
+        } else if (fecha.before(fecha2)){
+            System.out.println("Fecha Capturada por el usuario es anterior a la fecha actual");
+        } else if (fecha.equals(fecha2)){
+            System.out.println("Ambas fechas capturadas son iguales");
+        }
+        //Podemos comparar tambien con el CompareTo
+        //Colocamos una comparación -Si al comparar fecha vs fecha2 obtenemos un remanente, quiere decir que fecha2 es posterior
+        if(fecha.compareTo(fecha2)>0){
+            System.out.println("Fecha capturada es posterior a la fecha actual");
+        //-Si al comparar fecha vs fecha2 No obtenemos entero positivo, quiere decir que fecha2 es anterior a la fecha capturada por el usuario
+        } else if (fecha.compareTo(fecha2)<0){
+            System.out.println("Fecha capturada es anterior a la fecha actual");
+        //-Si es igual a 0, estamos ante una fecha igual.
+        } else if (fecha.compareTo(fecha2)==0){
+            System.out.println("Ambas fechas son iguales");
+        }
     }
 }
